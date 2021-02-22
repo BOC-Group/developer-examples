72a0bb1b2d1d2ae1261269255b36fd81c60aab0c
mathiaswinklhofer <mathias.winklhofer@boc-group.com>
2021-02-22 15:16:13 +0100

    Create GetConnection.java
    ADOXX GetConnection Tutorial for dev portal

---
+++ java/devportal/axx/GetConnection.java
@@ -0,0 +1,39 @@
+package devportal.axx;
+
+import java.io.IOException;
+
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+public class GetConnection
+{
+  public static void main (final String [] args) throws ClientProtocolException, IOException
+  {
+    final CloseableHttpClient aClient = HttpClients.createDefault ();
+    final String sPath = "http://<HOST>:<PORT>/ADOXX/rest/connection";
+    
+    final HttpGet aMethod = new HttpGet (sPath);
+    final CloseableHttpResponse aResponse = aClient.execute (aMethod);
+    
+    try
+    {
+      final String sResult = EntityUtils.toString (aResponse.getEntity (), "UTF-8");
+      final StatusLine aStatusLine = aResponse.getStatusLine ();
+      System.out.println ("Status Code: " + aStatusLine.getStatusCode ());
+      System.out.println ("Result: \n" + sResult);
+    }
+    catch (final Exception aEx)
+    {
+      aEx.printStackTrace ();
+    }
+    finally
+    {
+      aResponse.close ();
+    }
+  }
+}
