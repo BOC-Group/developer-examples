3ee08603e6280163762105f2f8acb6ec7a3cc7f6
mathiaswinklhofer <mathias.winklhofer@boc-group.com>
2021-02-23 10:06:05 +0100

    Create CreateArtifact.java

---
+++ java/devportal/ait/CreateArtifact.java
@@ -0,0 +1,77 @@
+package devportal.ait;
+
+import java.io.IOException;
+
+import javax.xml.bind.DatatypeConverter;
+
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+public class CreateArtifact
+{
+  public static void main (final String [] args) throws ClientProtocolException, IOException
+  {
+    final CloseableHttpClient aClient = HttpClients.createDefault ();
+    
+    final String sUser = "<USER>";
+    final String sPass = "<PASSWORD>";
+    final String sRepoID = "<REPO_ID>";
+    final String sPath = "http://<HOST>:<PORT>/ADOIT/rest/2.0/repos/" + sRepoID + "/objects";
+    
+    final HttpPost aMethod = new HttpPost (sPath);
+    
+    final StringBuilder aCreationParams = new StringBuilder ();
+    aCreationParams.append ("{");
+    aCreationParams.append ("\"name\":\"MyArtifact\",");
+    // Replace C_CLASSNAME with the name of your class
+    aCreationParams.append ("\"metaName\":\"C_ARTIFACT\",");
+    aCreationParams.append ("\"attributes\":");
+    aCreationParams.append ("[");
+    aCreationParams.append ("{");
+    // Replace A_STRING_ATTR with the name of your attribute
+    aCreationParams.append ("\"metaName\":\"A_DESCRIPTION\",");
+    aCreationParams.append ("\"value\":\"This is my artifact created via the API\"");
+    aCreationParams.append ("}");
+    aCreationParams.append ("]");
+    aCreationParams.append ("}");
+    
+    final StringEntity aJSON = new StringEntity (aCreationParams.toString (),
+                                                 ContentType.APPLICATION_JSON);
+    aMethod.setEntity (aJSON);
+    
+    // The headers controlling the return type and the language do not have to be considered for
+    // token generation
+    aMethod.addHeader ("Accept", "application/json");
+    aMethod.addHeader ("Accept-Language", "en");
+    
+    // Construction of the header to pass the basic authentication information
+    aMethod.addHeader ("Authorization",
+                       "Basic " +
+                           DatatypeConverter.printBase64Binary ((sUser + ":" + sPass).getBytes ()));
+    
+    final CloseableHttpResponse aResponse = aClient.execute (aMethod);
+    
+    try
+    {
+      final String sResult = EntityUtils.toString (aResponse.getEntity (), "UTF-8");
+      final StatusLine aStatusLine = aResponse.getStatusLine ();
+      System.out.println ("Status Code: " + aStatusLine.getStatusCode ());
+      System.out.println ("Result: \n" + sResult);
+    }
+    catch (final Exception aEx)
+    {
+      aEx.printStackTrace ();
+    }
+    finally
+    {
+      aResponse.close ();
+    }
+  }
+}
